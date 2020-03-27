c53b221f710656caf3d9768aba792c8e23f2618f
Abhishek Sharma <abhishek.sharma_cs18@gla.ac.in>
2020-03-27 23:57:01 +0530

    method for preorder

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -7,6 +7,8 @@
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
+import problem1.node.TreeNode;
+
 public class MyQueue {
     private Node front, end, tmp;
     private int size;
@@ -76,6 +78,15 @@ public class MyQueue {
             tmp.setNext(node);
             tmp = front;
         }
+    }
+
+    public void preorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        enqueue(new Node(node));
+        preorder(node.getLeft());
+        preorder(node.getRight());
     }
 
 }

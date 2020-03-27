6509601b9c7d29a38ca369da601002ef0babbc45
Abhishek Sharma <abhishek.sharma_cs18@gla.ac.in>
2020-03-27 22:58:32 +0530

    making getter and setter method

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -16,4 +16,28 @@ public class Node {
         this.next = null;
         this.priority = priority;
     }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 }

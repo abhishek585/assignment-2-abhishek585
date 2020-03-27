78d16aa2c58cb38cade133eb9760f1eece2de5d6
Abhishek Sharma <abhishek.sharma_cs18@gla.ac.in>
2020-03-27 20:41:52 +0530

    making the constructor.

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,8 +7,13 @@
 package problem1.node;
 
 public class TreeNode {
-    int data;
-    TreeNode left;
-    TreeNode right;
-    TreeNode root;
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
 }

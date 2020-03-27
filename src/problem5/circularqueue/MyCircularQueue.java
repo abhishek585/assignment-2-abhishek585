8b28817ef8345791dcf689a89607eb42f34f26f2
Abhishek Sharma <abhishek.sharma_cs18@gla.ac.in>
2020-03-28 00:16:58 +0530

    making constructor

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,16 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+import problem4.myqueue.Node;
+
 //to implement circular queue
 public class MyCircularQueue {
+    private Node front, end, tmp;
+
+    public MyCircularQueue() {
+        front = null;
+        end = null;
+        tmp = null;
+    }
 }

06ee8ccb75f2569d25b9886161be9937d2fd9880
Abhishek Sharma <abhishek.sharma_cs18@gla.ac.in>
2020-03-28 01:13:50 +0530

    getter and setter

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -25,4 +25,21 @@ public class Student {
         System.out.println("appering_count");
         this.apperingcount = sc.nextInt();
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
 }

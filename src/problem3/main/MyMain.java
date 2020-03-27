75ccdcb239497de96240215069a61e4e6b55e7f7
Abhishek Sharma <abhishek.sharma_cs18@gla.ac.in>
2020-03-27 23:26:46 +0530

    main class completed.

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,18 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert(40, 2);
+        obj.insert(10, 4);
+        obj.insert(30, 1);
+        obj.insert(20, 3);
+        obj.display();
+    }
 }
